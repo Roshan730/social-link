7e6e065a1396ff178ac2c38e11c3af3dc3722890
lucifer098a <nk5141030@gmail.com>
2023-04-27 21:37:48 +0530

    changes1

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:10000/users/${userId}/friends`,
+      `http://localhost:3001/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
@@ -38,9 +38,9 @@ const FriendListWidget = ({ userId }) => {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
+        {friends.map((friend, id) => (
           <Friend
-            key={friend._id}
+            key={id}
             friendId={friend._id}
             name={`${friend.firstName} ${friend.lastName}`}
             subtitle={friend.occupation}
