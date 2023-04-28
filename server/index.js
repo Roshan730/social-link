828153af4fa6b08bcd28b20e982e891ef8d5dccb
lucifer098a <nk5141030@gmail.com>
2023-04-28 10:05:37 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -53,7 +50,11 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 10000;
+app.get("/", (req, res) => {
+  res.send("<h1>Working fine</h1>");
+});
+
+const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
