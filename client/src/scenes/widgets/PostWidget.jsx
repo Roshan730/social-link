e9a8c45c3cf979ad90a559bfefac06a0102118ca
lucifer098a <nk5141030@gmail.com>
2023-05-01 12:06:47 +0530

    final changes

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -67,7 +67,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:10000/assets/${picturePath}`}
+          src={`https://social-link-backend.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
